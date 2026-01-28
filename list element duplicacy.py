37aa15f21e1a2447d94e98145acd5586c9c81092
Aj-0704 <agamjain9876@gmail.com>
2026-01-28 10:50:51 +0530

    new

---
+++ list element duplicacy.py
@@ -0,0 +1,6 @@
+l1=[1,2,3,4,5,1,6]
+l2=set(l1)
+if len(l1)==len(set(l2)):
+    print ("No Duplicates")
+else:
+    print("Duplicates Present")     
---
+++ string.py
@@ -0,0 +1,11 @@
+#format function
+a,b = 10,20
+print("sum of {} and {} is {}".format(a,b,a+b))
+#split function
+name = "agam jain"
+s = name.split(" ")
+print(s)
+#join function
+name = "agam jain"
+s = name.split(" ")
+print("".join(s))
